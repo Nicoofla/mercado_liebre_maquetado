2e65be5377bb2b5b68f12dcfc9aaf5ffe4950ef5
Nicoofla <nikoofla@gmail.com>
2023-10-12 19:52:18 -0300

    agrego home.html

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ const express = require("express");
 const app = express()
 const PORT = 3030;
 const path = require("path")
+
+
+app.use(express.static(path.join(__dirname,"public")))
+
+app.get("/",(req,res) => res.sendFile(path.join(__dirname,"views", "home.html")))
+
+app.listen(PORT, () => console.log(`Server running http://localhost:${PORT}`))
